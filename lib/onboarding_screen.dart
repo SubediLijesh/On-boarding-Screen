5fdc9b803f884f7a2a2c88b8f6469b8317d7bd3d
lijesh subedi <subedilijesh.com>
2023-11-30 09:27:33 +0545

    Initial Commit

---
+++ lib/onboarding_screen.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:onboarding/home_page.dart';
+import 'package:onboarding/intro_screens/Intro_page_2.dart';
+import 'package:onboarding/intro_screens/intro_page_1.dart';
+import 'package:onboarding/intro_screens/intro_page_3.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  //controller to keep on which page we are on
+  PageController _controller = PageController();
+  //keep track on if we are in last page or not
+  bool onLastPage = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            onPageChanged: (index) {
+              setState(() {
+                onLastPage = (index == 2);
+              });
+            },
+            controller: _controller,
+            children: [
+              IntroPage1(),
+              IntroPage2(),
+              IntroPage3(),
+            ],
+          ),
+          Container(
+            alignment: Alignment(0, 0.75),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //skip
+                GestureDetector(
+                  onTap: () {
+                    _controller.jumpToPage(2);
+                  },
+                  child: Text('Skip'),
+                ),
+                //dot indicator
+
+                SmoothPageIndicator(controller: _controller, count: 3),
+                //next
+                onLastPage
+                    ? GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return HomePage();
+                              },
+                            ),
+                          );
+                        },
+                        child: Text('Done'),
+                      )
+                    : GestureDetector(
+                        onTap: () {
+                          _controller.nextPage(
+                              duration: Duration(milliseconds: 500),
+                              curve: Curves.easeIn);
+                        },
+                        child: Text('Next'),
+                      ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
